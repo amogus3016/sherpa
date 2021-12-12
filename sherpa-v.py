91f9f092f636c0bf7dbe89ff714a2fa331780fef
amogus3016 <89612811+amogus3016@users.noreply.github.com>
2021-12-12 13:12:05 -0500

    Update sherpa-v.py

--- sherpa-v.py
+++ sherpa-v.py
@@ -1,6 +1,6 @@
 print ("Sherpa Terminal Emulator Version 0.1")
 print ("___________________")
-print ("| 		  |")
+print ("| 		            |")
 print ("| //              |")
 print ("|  //             |")
 print ("|   //            |")
@@ -8,3 +8,5 @@ print ("|  //             |")
 print ("| //              |")
 print ("|                 |")
 print ("|_________________|")
+
+# Can be executed by running 'python /path/to/sherpa-v.py', however creating an alias in your shell's rc file might be a good idea.
