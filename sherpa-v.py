f5924ac67e77b57394cf33832ad6fab59d294792
amogus3016 <89612811+amogus3016@users.noreply.github.com>
2021-12-17 11:46:32 -0500

    Update sherpa-v.py

--- sherpa-v.py
+++ sherpa-v.py
@@ -8,5 +8,7 @@ print ("|  //             |")
 print ("| //              |")
 print ("|                 |")
 print ("|_________________|")
+print ()
+print ("This program is provided as is, without any warranty. Refer to the GNU GPL for more information.")
 
 # Can be executed by running 'python /path/to/sherpa-v.py', however creating an alias in your shell's rc file might be a good idea.
