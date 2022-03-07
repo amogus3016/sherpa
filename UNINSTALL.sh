4df8cb9adcec41e071623596f7a51695b194c455
everest <liamwaldron7009@protonmail.com>
2022-03-07 21:26:41 +0000

    Create UNINSTALL.sh

---
+++ UNINSTALL.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Sherpa uninstallation script
+# This script uninstalls sherpa from the system.
+
+echo "Uninstalling sherpa..."
+rm /usr/bin/sherpa
+echo "rm /usr/bin/sherpa"
+echo "+----------------------------------------------+"
+echo "| Sherpa was uninstalled successfully.         |"
+echo "+----------------------------------------------+"
+# //
+#   //
+#    //
+#   //
+#  //
