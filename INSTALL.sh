3407211c0871525e4b2b86f882e757fed615a11a
everest <liamwaldron7009@protonmail.com>
2022-03-07 17:50:38 +0000

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,13 +3,14 @@
 # This script compiles the Sherpa source files into executable binaries, and moves them into their appropriate locations
 
 echo "Compiling Sherpa..."
-gcc -O2 -Wall $(pkg-config --cflags vte-2.91) sherpa.c -o Sherpa $(pkg-config --libs vte-2.91) # Compile main Sherpa binary
+gcc -O2 -Wall $(pkg-config --cflags vte-2.91) sherpa.c -o sherpa $(pkg-config --libs vte-2.91) # Compile main Sherpa binary
 echo "CC sherpa.c"
-echo "Compiling sherpa-version..."
-gcc -O2 -Wall sherpa-version.c # Compile the sherpa version script
-echo "CC sherpa-version.c"
-cp a.out sherpa-version
 mv sherpa /usr/bin
-mv sherpa-version /usr/bin
 echo "Sherpa was successfully installed."
 echo "To update Sherpa, run the UPDATE.sh script."
+
+//
+  //
+   //
+  //
+ //
